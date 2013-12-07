4e40437d8d1c4b985d44a624e733e6a1bf7c522a
Graham Cox <graham@grahamcox.co.uk>
2013-12-07 11:08:29 +0000

    Some tidying up of generated files

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ var configData = {};
 
 configData.general = {
   apiVersion: "0.0.1",
-  serverName: "actionHero API",
+  serverName: "GMWars",
   // id: "myActionHeroServer",                                    // id can be set here, or it will be generated dynamically.  Be sure that every server you run has a unique ID (which will happen when genrated dynamically)
-  serverToken: "change-me",                                       // A unique token to your application that servers will use to authenticate to each other
-  welcomeMessage : "Hello! Welcome to the actionHero api",        // The welcome message seen by TCP and webSocket clients upon connection
+  serverToken: "269b9149-429d-4353-8bce-03bb7414c116",            // A unique token to your application that servers will use to authenticate to each other
+  welcomeMessage : "Hello! Welcome to the GMWars api",            // The welcome message seen by TCP and webSocket clients upon connection
   flatFileNotFoundMessage: "Sorry, that file is not found :(",    // The body message to accompany 404 (file not found) errors regading flat files
   serverErrorMessage: "The server experienced an internal error", // The message to accompany 500 errors (internal server errors)
   defaultChatRoom: "defaultRoom",                                 // The chatRoom that TCP and webSocket clients are joined to when the connect
@@ -133,7 +133,7 @@ configData.servers = {
       'Access-Control-Allow-Headers': 'Content-Type'
     },    
     urlPathForActions : "api",           // Route that actions will be served from; secondary route against this route will be treated as actions, IE: /api/?action=test == /api/test/
-    urlPathForFiles : "public",          // Route that static files will be served from; path (relitive to your project root) to server static content from
+    urlPathForFiles : "",                // Route that static files will be served from; path (relitive to your project root) to server static content from
     rootEndpointType : "api",            // When visiting the root URL, should visitors see "api" or "file"? Visitors can always visit /api and /public as normal
     directoryFileType : "index.html",    // The default filetype to server when a user requests a directory
     flatFileCacheDuration : 60,          // The header which will be returned for all flat file served from /public; defined in seconds
