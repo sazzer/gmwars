b40875105121cf65040345c8a4172ba4edec5a98
Graham Cox <graham.cox@oclc.org>
2014-01-09 13:06:18 +0000

    Added link for Google Auth. Not working yet

--- public/js/view/login.js
+++ public/js/view/login.js
@@ -3,9 +3,10 @@
  */
 define([
     "dijit/Dialog",
+    "dojo/on",
     "dojo/i18n",
     "dojo/i18n!./nls/login"
-], function(Dialog, i18n) {
+], function(Dialog, on, i18n) {
     var strings = i18n.getLocalization("gmwars.view", "login", this.lang);
     return {
         /**
@@ -13,11 +14,24 @@ define([
          */
         display: function() {
             console.log("Displaying the login dialog");
+            var content = [
+                "<a href='#' class='loginLink' rel='google'>",
+                    "Log in with Google",
+                "</a>"
+            ].join("");
             var loginDialog = new Dialog({
                 title: strings.dialogTitle,
-                content: strings.dialogTitle,
                 style: "width: 300px",
                 closable: false
+            });
+            loginDialog.set("content", content);
+
+            on(dojo.query(".loginLink", loginDialog.get("containerNode")), "click", function(evt) {
+                evt.preventDefault();
+
+                var provider = evt.currentTarget.getAttribute("rel");
+                console.log("Logging in with " + provider);
+                return false;
             });
             loginDialog.show();
         }
